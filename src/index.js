1f1d6d66cb440376f705b393008c9451bc5cad0f
ypk <yprasannakumar@gmail.com>
2022-09-01 20:54:03 +0100

    upgraded dependencies and switched to react 18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Board } from './components/board'
 import configureStore from './store'
 import { Provider } from 'react-redux'
 
 const store = configureStore()
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Board />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+  </React.StrictMode>)
